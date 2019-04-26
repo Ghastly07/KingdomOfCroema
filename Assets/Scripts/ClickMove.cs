859e3d28dba45878d8256fc5c3f5b48da9820062
Pansik <piotrwesolowski22@gmail.com>
2019-04-26 11:28:18 +0200

    Add - Materials folder - Scripts folder Create - Debug materials for ground, player and obstackles - Debug scene for testing player movement - Player movement on mouse click - Smooth camera follow player - Nav mesh for debug scene - Walkable layer

---
+++ Assets/Scripts/ClickMove.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ClickMove : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask clickableLayer;
+
+    [SerializeField]
+    Camera myCamera;
+
+    [SerializeField]
+    float rayDistance = 200;
+
+    private NavMeshAgent navMeshAgent;
+
+    private void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, rayDistance, clickableLayer))
+            {
+                navMeshAgent.SetDestination(hit.point);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Vector3 offset;
+
+    public float smoothSpeed;
+
+    private void LateUpdate()
+    {
+        if(target != null)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
+    }
+}
