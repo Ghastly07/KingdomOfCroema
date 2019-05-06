d841c6b516fb1c21e5c0693fd72fcd3b0a77eea8
Pansik <piotrwesolowski22@gmail.com>
2019-05-07 00:41:09 +0200

    Cleanup

--- Assets/Scripts/AttackSMB.cs
+++ Assets/Scripts/AttackSMB.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class AttackSMB : StateMachineBehaviour
 {
-    public CombatController combatController;
     public int attackNumber;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
